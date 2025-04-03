8c01e73e16a377e6f91c97f0df2350b9bc1f0999
Bry <etoquillab@gmail.com>
2025-04-03 10:14:42 +0800

    add css, js and fix the allowed host

--- projectsite/settings.py
+++ projectsite/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'django-insecure-$(dezv_)==(*+dy4x!)bobmxeg2soo(lm=&%6(2oxn@x^w9^o-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['psusphere.pythonanywhere.com', '127.0.0.1']
+ALLOWED_HOSTS = ["bry25.pythonanywhere.com", "127.0.0.1", "localhost"]
+
+
 
 
 # Application definition
