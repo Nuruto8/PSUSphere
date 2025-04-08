3e94f4da431a7e8421b03ad2d863d9f4c2125dd2
Bry <etoquillab@gmail.com>
2025-04-08 13:21:52 +0800

    add form

--- studentorg/forms.py
+++ studentorg/forms.py
@@ -1,9 +1,29 @@
 from django.forms import ModelForm
 from django import forms
-from .models import Organization
+from .models import College, Organization, Student, Program, OrgMember
+
+# ✅ Added College Form
+class CollegeForm(ModelForm):
+    class Meta:
+        model = College
+        fields = "__all__"
 
 class OrganizationForm(ModelForm):
     class Meta:
         model = Organization
         fields = "__all__"
-        
+
+class StudentForm(ModelForm):
+    class Meta:
+        model = Student
+        fields = "__all__"
+
+class ProgramForm(ModelForm):
+    class Meta:
+        model = Program
+        fields = "__all__"
+
+class OrgMemberForm(ModelForm):
+    class Meta:
+        model = OrgMember
+        fields = "__all__"
--- studentorg/views.py
+++ studentorg/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from studentorg.models import College, Organization, Student, Program, OrgMember
-from studentorg.forms import CollegeForm, OrganizationForm, StudentForm, ProgramForm, OrgMemberForm
+from studentorg.forms import (
+    CollegeForm, OrganizationForm, StudentForm, ProgramForm, OrgMemberForm
+)
 from django.urls import reverse_lazy
 
 class HomePageView(ListView):
