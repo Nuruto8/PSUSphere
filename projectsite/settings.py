e3f6e9456abf1b5a569c724c45d9425978116d0d
Bry <etoquillab@gmail.com>
2025-04-10 12:32:52 +0800

    auth

--- projectsite/settings.py
+++ projectsite/settings.py
@@ -94,24 +94,22 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
+
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': '''django.contrib.auth.password_validation.
-        UserAttributeSimilarityValidator''',
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
     {
-        'NAME': '''django.contrib.auth.password_validation.
-        MinimumLengthValidator''',
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
     },
     {
-        'NAME': '''django.contrib.auth.password_validation.
-        CommonPasswordValidator''',
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
     },
     {
-        'NAME': '''django.contrib.auth.password_validation.
-        NumericPasswordValidator''',
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
 
 
 # Internationalization
@@ -135,3 +133,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGOUT_REDIRECT_URL = 'login'
+LOGIN_REDIRECT_URL = 'home'
+LOGIN_URL = 'login'
