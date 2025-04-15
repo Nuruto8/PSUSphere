48a0535a0530125c2fac07e3d20d725ebf48ad9c
Bry <etoquillab@gmail.com>
2025-04-15 11:17:26 +0800

    new add

--- studentorg/admin.py
+++ studentorg/admin.py
@@ -6,7 +6,7 @@ admin.site.register(Program)
 admin.site.register(Organization)
 
 
-@admin.register(Student)  # Keep this and remove 'admin.site.register(Student)'
+@admin.register(Student)  
 class StudentAdmin(admin.ModelAdmin):
     list_display = ("student_id", "lastname", "firstname", "middlename", "program")
     search_fields = ("lastname", "firstname",)
